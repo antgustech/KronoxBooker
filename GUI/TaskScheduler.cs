161ef852c30e4538ea8686beaa6b789ee1118608
Anton Gustafsson <anton.gustafsson@protonmail.com>
2017-07-24 12:34:40 +0200

    updated task sheduling to be more robust

--- GUI/TaskScheduler.cs
+++ GUI/TaskScheduler.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Win32.TaskScheduler;
 using System;
+using System.Diagnostics;
 using System.IO;
 
 namespace KronoxScraperBotGUI
@@ -12,16 +13,23 @@ namespace KronoxScraperBotGUI
         internal void AddTask()
         {
             var now = DateTime.Now;
-            var t = new DateTime(now.Year, now.Month, now.Day, 0, 0, 01).AddDays(1);
+            var midnightNextDay = new DateTime(now.Year, now.Month, now.Day, 0, 0, 0).AddDays(1);
+            var oneMinute = new TimeSpan(0, 00, 01, 0); 
+            var oneHour = new TimeSpan(0, 01, 00, 0); 
 
             using (TaskService ts = new TaskService())
             {
                 var td = ts.NewTask();
                 td.RegistrationInfo.Description = "This task will start the bot booking application at midnight.";
-                td.Triggers.Add(new TimeTrigger(t));
+                td.Triggers.Add(new TimeTrigger(midnightNextDay));
                 td.Settings.AllowDemandStart = true;
                 td.Settings.WakeToRun = true;
                 td.Settings.RestartCount = 3;
+                td.Settings.DisallowStartIfOnBatteries = false;
+                td.Settings.StopIfGoingOnBatteries = false;
+                td.Settings.StartWhenAvailable = true;
+                td.Settings.RestartInterval = oneMinute;
+                td.Settings.ExecutionTimeLimit = oneHour;
                 td.Actions.Add(new ExecAction(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)) + "/Kronox Bot/files/Bot.exe", null, null));
                 ts.RootFolder.RegisterTaskDefinition(@"StartKronoxBotBooker", td);
             }
